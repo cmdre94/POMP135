677783f5d0cfbe286292693e811a5155f7d627f7
Andre Mauldin <cam94@att.net>
2024-04-22 21:44:54 -0500

    added routing to button on homepage

--- src/app/views/homepage-view/homepage-view.component.ts
+++ src/app/views/homepage-view/homepage-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-homepage-view',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage-view.component.css']
 })
 export class HomepageViewComponent implements OnInit {
-
-  constructor() { }
+  public activeRoute: string;
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * @description - Routes to the passed in view
+   * @param {string} url - the URL to route to
+   * @returns {void}
+   */
+  public routeToView(url: string): void {
+    this.router.navigate([url]);
+    this.activeRoute = url;
   }
 
 }
